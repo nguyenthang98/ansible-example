4db15ebe7a8010a4e9200096a0bcc3fef2048920
Nguyen Duc Thang <nguyenducthang.uet@gmail.com>
2020-08-16 15:46:04 +0700

    update

--- provision-scripts/configure-static-ip-centos.sh
+++ provision-scripts/configure-static-ip-centos.sh
@@ -9,9 +9,13 @@ BOOTPROTO="static"
 IPADDR=$1
 NETMASK=255.255.255.0
 GATEWAY=192.168.100.1
+DNS1=8.8.8.8
+DNS2=4.4.4.4
 EOF
 
-cat << EOF >> /etc/resolve.conf
+cat << EOF >> /etc/resolv.conf
 nameserver 8.8.8.8
 nameserver 4.4.4.4
 EOF
+
+sed -i "s/PasswordAuthentication\ no/PasswordAuthentication\ yes/g" /etc/ssh/sshd_config
